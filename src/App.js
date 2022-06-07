95535bd9039d84fbce915ea105c3bcdfa277ca16
aaron-portuphy <72166690+aaron-portuphy@users.noreply.github.com>
2022-06-07 19:30:37 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/common/header/Header";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          {/*<Route path="/about">
+            <About />
+            </Route>*/}
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
